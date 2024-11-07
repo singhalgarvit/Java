b16e484d5434ae5605b969545e5d7bf6a5ffeb28
Garvit Singhal <garvitsinghal305@gmail.com>
2024-11-08 01:11:07 +0530

    Day 01 Of Java

---
+++ Grade.java
@@ -0,0 +1,17 @@
+public class Grade {
+    public static void main(String[] args){
+        int marks=9;
+        if(marks>90){
+            System.out.println("Excellent");
+        } 
+        else if(marks>80){
+            System.out.println("Great");
+        }
+        else if(marks>60){
+            System.out.println("Good");
+        }
+        else{
+            System.out.println("Better Luck Next Time");
+        }
+    }
+}
