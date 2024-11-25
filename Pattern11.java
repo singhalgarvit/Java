9d19148af8975c5cc06b97e1a41336bfea8f2cd9
Garvit Singhal <garvitsinghal305@gmail.com>
2024-11-25 23:45:00 +0530

    Day 09 of Java

---
+++ Pattern11.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Pattern11 {
+
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int sp=n/2;
+        int st=1;
+        int a=1;
+        for(int i=1;i<=n;i++){
+            int b=a;
+            for(int j=1;j<=sp;j++){
+                System.out.print("\t");
+            }
+            for(int k=1;k<=st;k++){
+                System.out.print(b+"\t");
+                if(k<=(st/2)){
+                    b++;
+                }
+                else{
+                    b--;
+                }
+            }
+            System.out.println();
+
+            if(i<=(n/2)){
+                sp--;
+                st = st+2;
+                a++;
+            }
+            else{
+                sp++;
+                st = st-2;
+                a--;
+            }
+        }
+    }
+}
