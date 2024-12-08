3c636416b130994d8c3b63e0abca8c790dfe05c4
Garvit Singhal <garvitsinghal305@gmail.com>
2024-12-08 23:42:40 +0530

    Day 15 of Java

---
+++ Arrays/InverseAnArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class InverseAnArray {
+
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+        int[] inv = new int[n];
+        for(int i=0;i<n;i++){
+            int c=arr[i];
+            inv[c]=i;
+        }
+        for(int j=0;j<n;j++){
+            System.out.println(inv[j]);
+        }
+    }
+}
